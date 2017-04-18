0d98350b7d153edd0a378b6f824be58e4d06bab2
Paulo Costa <me@paulo.costa.nom.br>
2017-04-18 16:47:04 -0300

    StubManifest: put 'final' on constants

--- VirtualApp/lib/src/main/java/com/lody/virtual/client/stub/StubManifest.java
+++ VirtualApp/lib/src/main/java/com/lody/virtual/client/stub/StubManifest.java
@@ -8,13 +8,13 @@ import java.util.Locale;
 
 public class StubManifest {
     public static final String STUB_DEF_AUTHORITY = "virtual_stub_";
-    public static String STUB_ACTIVITY = StubActivity.class.getName();
-    public static String STUB_DIALOG = StubDialog.class.getName();
-    public static String STUB_CP = StubContentProvider.class.getName();
-    public static String STUB_JOB = StubJob.class.getName();
-    public static String RESOLVER_ACTIVITY = ResolverActivity.class.getName();
+    public static final String STUB_ACTIVITY = StubActivity.class.getName();
+    public static final String STUB_DIALOG = StubDialog.class.getName();
+    public static final String STUB_CP = StubContentProvider.class.getName();
+    public static final String STUB_JOB = StubJob.class.getName();
+    public static final String RESOLVER_ACTIVITY = ResolverActivity.class.getName();
     public static String STUB_CP_AUTHORITY = "virtual_stub_";
-    public static int STUB_COUNT = 50;
+    public static final int STUB_COUNT = 50;
 
     /**
      * If enable,
